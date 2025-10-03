1a2b59ae19db0b896fc5351ea89d4652fd1d6cc9
Lucas1479 <sli776@auckland.uni.ac.nz>
2025-10-04 00:01:51 +1300

    Fixed_URL

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastProvider } from './context/ToastContext';
+import axios from 'axios';
+
+// Set a global axios baseURL so all relative requests go to the backend domain
+axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
